90f0fef65adef4cab3748109107469913a0b54a7
Rudy <rudydimacali@gmail.com>
2019-01-02 11:25:35 -0800

    Implement axios getting calendar component. TODO: Render.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+const axios = require('axios');
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     };
+  }
+
+  componentDidMount() {
+    const id = document.URL.split('/')[4];
+    axios.get(`/buildPage/restaurants/${id}/`)
+      .then(response => {
+        console.log(response.data);
+        console.log(ReactHtmlParser(response.data)[1].props.children[1]);
+        ReactDOM.render(<div id='calendar'>{ReactHtmlParser(response.data)[1].props.children[1]}</div>, document.getElementById('app'));
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 app.use(bodyParser());
+
+app.get('/buildPage/restaurants/:id/', (req, res) => {
+  axios.get(`http://localhost:3002/restaurants/${req.params.id}/`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      throw err;
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
