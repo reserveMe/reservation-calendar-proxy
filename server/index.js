ab189429bef98599a7df947a033edf48fe2608c8
Rudy <rudydimacali@gmail.com>
2019-01-04 20:12:39 -0800

    Deploy proxy to ec2.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware')
 
 const app = express();
 
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 app.use(bodyParser());
+
+app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
+app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
+app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://54.183.92.72:3004', changeOrigin: true }));
 
 const port = 3000;
 app.listen(port, () => {
