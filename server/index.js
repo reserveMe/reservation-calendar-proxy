165e3555c0e232b5ea2f52461f304fd9214c2a0a
Rudy <rudydimacali@gmail.com>
2019-01-08 10:21:07 -0800

    Final push.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ app.use(bodyParser());
 app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
 app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
 app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://54.183.92.72:3004', changeOrigin: true }));
+app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
+app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
 
 const port = 3000;
 app.listen(port, () => {
